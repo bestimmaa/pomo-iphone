6a3cc1583d65deb844ec50863485150ba536e872
Christoph Halang <christoph.halang@gmail.com>
2016-06-04 16:29:36 +0200

    setup xcode project

---
+++ gettext-ios/gettext_ios.h
@@ -0,0 +1,13 @@
+//
+//  gettext_ios.h
+//  gettext-ios
+//
+//  Created by Christoph Halang on 04/06/16.
+//  Copyright © 2016 Klickrent GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface gettext_ios : NSObject
+
+@end
---
+++ gettext-ios/gettext_ios.m
@@ -0,0 +1,13 @@
+//
+//  gettext_ios.m
+//  gettext-ios
+//
+//  Created by Christoph Halang on 04/06/16.
+//  Copyright © 2016 Klickrent GmbH. All rights reserved.
+//
+
+#import "gettext_ios.h"
+
+@implementation gettext_ios
+
+@end
