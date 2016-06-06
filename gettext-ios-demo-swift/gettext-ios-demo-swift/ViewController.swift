0b4cf7de78baea073fd8da0813fad6611d55c707
Christoph Halang <christoph.halang@gmail.com>
2016-06-06 11:43:22 +0200

    added demo project for swift

---
+++ gettext-ios-demo-swift/gettext-ios-demo-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gettext-ios-demo-swift
+//
+//  Created by Christoph Halang on 06/06/16.
+//  Copyright © 2016 Klickrent GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gettextios/gettextios.h
@@ -0,0 +1,19 @@
+//
+//  gettextios.h
+//  gettextios
+//
+//  Created by Christoph Halang on 04/06/16.
+//  Copyright © 2016 Klickrent GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gettextios.
+FOUNDATION_EXPORT double gettextiosVersionNumber;
+
+//! Project version string for gettextios.
+FOUNDATION_EXPORT const unsigned char gettextiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gettextios/PublicHeader.h>
+
+
